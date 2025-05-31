324d63e4f385e217cde1220bc25fbd13b6300774
BVE_I_ <metria2018@icloud.com>
2025-05-31 23:02:30 +0900

    振れすぎるのを調整

--- SpeedMeter.h
+++ SpeedMeter.h
@@ -15,10 +15,12 @@ public:
 		Update = (bool)((Time / Interval) % 2);
 		std::random_device seed;
 		std::mt19937 engine(seed());
-		double mu = 0.0, sig = 0.25;
+		double mu, sig;
+		if (Increment < 1)mu = 0.0, sig = 0.12;
+		else mu = 0.0, sig = 0.25;
 		std::normal_distribution<> dist(mu, sig);
 		double rand = dist(engine);
-		double k = -exp(-fabs(Speed) / 15) + 1;
+		double k = -exp(-fabs(Speed) / 25) + 1;
 		if (Update && !(UpdateOld))SaveSpeed = fabsf(Speed + k * rand);
 		ToReturnMeter = (Increment > 1) ? (int(SaveSpeed / Increment)) * Increment : (int(SaveSpeed / Increment));
 		ToReturnHundredsPlace = (SaveSpeed < 100) ? 10 :(int)SaveSpeed / 100;
